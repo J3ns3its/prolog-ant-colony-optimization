45b60f636292a3900f7c67b4c3e9fc83d233fa41
Jens Hartmann <hartmannje@cip.ifi.lmu.de>
2021-02-25 13:35:08 +0100

    runMethoden ueberarbeitet, Koordinaten werden Normiert, arcs werden generiert

--- aco.pl
+++ aco.pl
@@ -1,50 +1,21 @@
-% https://nerddan.github.io/2015/01/07/prolog-tsp.html
-arc(a, b, 10).
-arc(a, c, 2).
-arc(a, d, 9).
-arc(a, e, 11).
-arc(b, c, 8).
-arc(b, d, 12).
-arc(b, e, 6).
-arc(c, d, 3).
-arc(c, e, 8).
-arc(d, e, 9).
+:- ['arcs.pl'].
 
 edgeC(X, Y, Weight) :- arc(X, Y, Weight) ; arc(Y, X, Weight).
 edgeP(X, Y, Prob) :- prob(X, Y, Prob) ; prob(Y, X, Prob).
 edgeT(X, Y, Tau) :- int(X, Y, Tau) ; int(Y, X, Tau).
-
-alpha(1).
-beta(1).
-persistance(0.5).
-constantQ(1).
-nodes([a,b,c,d,e]).
-totalAnts(5).
 /*****************************************************************************/
 
-path([_], 0).
-path([X, Y | Tail], Cost) :-
-    edgeC(X, Y, Cost0),
-    path([Y | Tail], Cost1),
-    \+ member(X, Tail),
-    Cost is Cost0+Cost1.
+alpha(1).
+beta(5).
+persistance(0.5).
+constantQ(100).
 
-hami_cycle([a | Tail], Cost) :-
-    length(Tail, 4),
-    edgeC(a, Last, Cost0),
-    last(Tail, Last),
-    path([a | Tail], Cost1),
-    Cost is Cost0+Cost1,
-    write("tst: "), write([a|Tail]), write("; C = "), writeln(Cost).
+totalAnts(100).
 
-min_hami_cycle(Path, Cost) :-
-	hami_cycle(Path, Cost),
-	write("min: "), write(Path), write("; C = "), writeln(Cost),
-    \+ (hami_cycle(_, T), T<Cost).
 /*****************************************************************************/
 % NPs: Node:Probability list of unvisited neighours:
 choice(X, Unvisited0, Y) :-
-    setof(N:P, (member(N, Unvisited0), edgeP(X,N,P)), NPs),
+    setof(N:P, (member(N, Unvisited0), edgeP(X, N, P)), NPs),
     sumP(NPs, 0, Pges),
     random(0, Pges, Tar),
     roll(NPs, Tar, 0, Y).
@@ -66,10 +37,10 @@ roll([_:P|NPs], Tar, AkkIn, Y) :-
     AkkOut is AkkIn + P, roll(NPs, Tar, AkkOut, Y).
 
 % Hilfsfunktion: path/4 wuerfelt einen Pfad aus und berechnet die Kosten:
-path(X, [Y], [X,Y], C) :- edgeC(X,Y,C).
+path(X, [Y], [X,Y], C) :- edgeC(X, Y, C).
 path(X, Unvisited0, [X|Path], SumCost0) :-
         choice(X, Unvisited0, Y),
-        edgeC(X,Y,C),    
+        edgeC(X, Y, C),    
 	selectchk(Y, Unvisited0, Unvisited1),
 	path(Y, Unvisited1, Path, SumCost1),
 	SumCost0 is SumCost1 + C.
@@ -80,7 +51,7 @@ path(X, Unvisited0, [X|Path], SumCost0) :-
 % X ist Startwert, die Reihenfolge der anderen Unvisited elemente ist egal
 path([X|Unvisited], Path, SumCost0) :-
 	path(X, Unvisited, Path, SumCost1),
-	% add cost for closing the path:
+	% Kosten um den Pfad abzuschließen:
 	last(Path, Last),
 	edgeC(X, Last, C),
 	SumCost0 is SumCost1 + C,
@@ -88,93 +59,105 @@ path([X|Unvisited], Path, SumCost0) :-
 /*****************************************************************************/
 
 runACO(SolPath, SolCost) :-
-    findall(X:Y, arc(X,Y,_), XYs), initTP(XYs),
-    assert(bestSol([],2^31-1)),
-    bestSol(_, OldCost), runCycle,
-    writeln("   reRun?"), reRun(OldCost),    
+    findall(X:Y, arc(X, Y, _), XYs), initTP(XYs),
+    assert(bestSol([], 2^31-1)),
+    aux,
     bestSol(SolPath, SolCost), !.
 
-runCycle() :-
-    findall(X:Y, arc(X,Y,_), XYs), evaporateT(XYs),
+aux :-
+    bestSol(_, OldCost),
+    runCycleMultiple(5),
+    reRun(OldCost).
+    
+reRun(OldCost) :-
+    writeln("   reRun?"), 
+    bestSol(_, NewCost),
+    write("   OldCost: "), write(OldCost), write(" NewCost: "), writeln(NewCost),
+    NewCost < OldCost, aux.
+reRun(_).
+
+runCycleMultiple(0).
+runCycleMultiple(Count) :-
+    Count1 is Count -1, runCycle, runCycleMultiple(Count1).
+
+runCycle :-
+    findall(X:Y, arc(X, Y, _), XYs), evaporateT(XYs),
     totalAnts(M), runAllAnts(M),
     mapUpdateP(XYs).
-
-reRun(OldCost) :-
-    bestSol(_, NewCost),
-    write("   OldCost: "),write(OldCost),write(" NewCost: "),writeln(NewCost),
-    NewCost < OldCost, runCycle.
-reRun(_).
 
 runAllAnts(0).
 runAllAnts(Count) :- 
     runAnt, Count1 is Count -1,
     runAllAnts(Count1).
 
+%%% Für eine Ameise mit zufälligen Startpunkt wir ein Pfad ermittelt.
+%%% Anschließend werden die Pheromone aktualisiert.
 runAnt :-
     nodes(Nodes),
     random_select(Start, Nodes, Rest),    
     path([Start|Rest], Path, C), updateC(Path, C),
-    last(Path, Last), updateT(Start, Last, C), mapUpdateT(Path, C),
-    write("   Path: "), write(Path), write("   Cost: "), writeln(C).    
+    last(Path, Last), updateT(Start, Last, C), mapUpdateT(Path, C).
+%    write("   Path: "), write(Path), write("   Cost: "), writeln(C).    
 
 
 /*****************************************************************************/
-mapUpdateT([_], _).
-mapUpdateT([X,Y|Path], C) :- updateT(X,Y,C), mapUpdateT([Y|Path], C).
+%%% Initialisieren und Updaten von Tau, Preferenz und Cost.
 
-updateT(X,Y,C) :-
+initTP([]).
+initTP([X:Y|XYs]) :- assert(int(X, Y, 1)), updateP(X, Y), initTP(XYs).
+
+mapUpdateT([_], _).
+mapUpdateT([X, Y|Path], C) :- updateT(X, Y, C), mapUpdateT([Y|Path], C).
+
+updateT(X, Y, C) :-
     edgeT(X, Y, TauOld), constantQ(Q),
     TauNew is TauOld + Q/C,
-    assertT(X,Y,TauNew).
+    assertT(X, Y, TauNew).
 
 evaporateT([]).
 evaporateT([X:Y|XYs]) :-
-    edgeT(X,Y, TauOld), persistance(P),
-    TauNew is TauOld * P, assertT(X,Y,TauNew),
+    edgeT(X, Y, TauOld), persistance(P),
+    TauNew is TauOld * P, assertT(X, Y, TauNew),
     evaporateT(XYs).
 
-assertT(X,Y, TauNew) :-			
-    retractall(int(X,Y,_)),retractall(int(Y,X,_)),
-    assert(int(X,Y, TauNew)).
+assertT(X, Y, TauNew) :-			
+    retractall(int(X, Y, _)), retractall(int(Y, X, _)),
+    assert(int(X, Y, TauNew)).
 
 mapUpdateP([]).
-mapUpdateP([X:Y|XYs]) :- updateP(X,Y), mapUpdateP(XYs).
+mapUpdateP([X:Y|XYs]) :- updateP(X, Y), mapUpdateP(XYs).
 
-%Probabilities sind nicht normiert
-updateP(X,Y) :-
+%%% Preferenzen sind nicht normiert
+updateP(X, Y) :-
     alpha(A), beta(B), 
-    edgeT(X,Y, Tau), edgeC(X,Y,C), P is Tau^A * (1/C)^B,
-    retractall(prob(X,Y,_)),
-    assert(prob(X,Y,P)).    
+    edgeT(X, Y, Tau), edgeC(X, Y, C),
+    P is Tau^A * (100/C)^B,
+    retractall(prob(X, Y, _)), assert(prob(X, Y, P)).    
     
-updateC(_, C) :- bestSol(_,Cbest), C >= Cbest.
+updateC(_, C) :- bestSol(_, Cbest), C >= Cbest.
 updateC(Path, C) :-
-    bestSol(_,Cbest), C < Cbest,
-    write("   UPDATECOST: "), write(Cbest), write(" ---> "), writeln(C), writeln(""), 
+    bestSol(_, Cbest), C < Cbest,
+    write("   UPDATE Best Cost: "), write(Cbest), write(" ---> "), writeln(C), writeln(""), 
     retractall(bestSol(_,_)), assert(bestSol(Path, C)).
 
-
-%%% Funktionen zum Testen der Initialisierung
-initTP([]).
-initTP([X:Y|XYs]) :- assert(int(X,Y, 1)), updateP(X,Y), initTP(XYs).
 
 
 /*****************************************************************************/
 %%% Hilfsfunktionen zur Ausgabe
-round(X,Y,D) :- Z is X * 10^D, round(Z, ZA), Y is ZA / 10^D.
+round(X, Y, D) :- Z is X * 10^D, round(Z, ZA), Y is ZA / 10^D.
 
 % Pretty Print von Listen
-pp([H|T],I) :- !, J is I+3, pp(H,J), ppx(T,J),nl.
-pp(X,I):- tab(I), write(X), nl.
+pp([H|T], I) :- !, J is I+3, pp(H, J), ppx(T, J),nl.
+pp(X, I):- tab(I), write(X), nl.
 ppx([],_).
-ppx([H|T],I):-pp(H,I),ppx(T,I).
+ppx([H|T], I):-pp(H, I),ppx(T, I).
 
 dotGraph :-
     tell(ausgabeTsp),
     writeln("digraph G {\nnode [ordering=\"out\"]"),
-    writeln("node [shape=none, height=0.25, fixedsize=\"true\"]"),    
+    writeln("node [shape=circle, height=0.25, fixedsize=\"true\"]"),    
     nodes(Nodes), dotNode(Nodes),
-    findall(X:Y, arc(X,Y,_), XYs), dotEdge(XYs),
+    findall(X:Y, arc(X, Y, _), XYs), dotEdge(XYs),
     write("}"),
     told, !.
 
@@ -185,10 +168,10 @@ dotNode([N|Nodes]) :-
 
 dotEdge([]).
 dotEdge([X:Y|XYs]) :-
-    edgeC(X,Y,C), edgeT(X,Y,T), edgeP(X,Y,P),
-    round(T,TR,3), round(P,PR,3), 
+    edgeC(X, Y, C), edgeT(X, Y, T), edgeP(X, Y, P),
+    round(T, TR, 3), round(P, PR, 3), CR is round(C),
     write(X), write(" -> "), write(Y), write(" [label=\""),
-    write("C:"), writeln(C),
+    write("C:"), writeln(CR),
     write("T:"), writeln(TR),
     write("P:"), writeln(PR),    
     writeln("\"]"),
---
+++ generateArcs.pl
@@ -0,0 +1,49 @@
+normTowns :-
+    findall(X ,town(_, X, _), Xs),    
+    min_member(LowX, Xs),
+    max_member(MaxX, Xs),
+    NormX is MaxX - LowX,
+
+    findall(Y ,town(_, _, Y), Ys),
+    min_member(LowY, Ys),
+    max_member(MaxY, Ys),
+    NormY is MaxY - LowY,
+    
+    write("   LowX: "), writeln(LowX),
+    write("   NormX: "), writeln(NormX),
+    write("   LowY: "), writeln(LowY),
+    write("   NormY: "), writeln(NormY),    
+    tell('townNorm.pl'),
+    findall(ID:X:Y, town(ID,X,Y), Towns), normSingleTown(Towns, NormX, LowX, NormY, LowY),
+    told,
+    genArcs, !.
+
+normSingleTown([], _, _, _, _).
+normSingleTown([ID:X:Y|IDXY], NormX, LowX, NormY, LowY) :-
+    XN is 100*((X - LowX) / NormX), YN is 100*((Y - LowY) / NormY),
+%    write("   normSingleTown: "), writeln(ID), writeln(XN), writeln(YN),    
+    write("townN("), write(ID), write(", "), write(XN),
+    write(", "), write(YN), writeln(")."),
+    normSingleTown(IDXY, NormX, LowX, NormY, LowY).
+
+genArcs :-
+    ['townNorm.pl'],
+    tell('arcs.pl'),
+    findall(ID:X:Y, townN(ID,X,Y), Towns), addArcs(Towns),
+    findall(ID1, townN(ID1,_,_), IDs),
+    writeln(""), write("nodes("), write(IDs), writeln(")."),
+    told, !.
+
+
+addArcs([]).
+addArcs([ID1:X1:Y1|IDXY]) :-
+    addSingleArc(ID1, X1, Y1, IDXY),
+    addArcs(IDXY).
+
+addSingleArc(_, _, _, []).
+addSingleArc(ID1, X1, Y1, [ID2:X2:Y2|IDXY]) :-
+%    write("   addSingleArc: "), writeln(ID1), writeln(ID2), writeln(IDXY),
+    C is sqrt((X1 - X2)^2 + (Y1 - Y2)^2),
+    write("arc("), write(ID1), write(", "), write(ID2),
+    write(", "), write(C), writeln(")."),
+    addSingleArc(ID1, X1, Y1, IDXY).
