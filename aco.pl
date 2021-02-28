8ca9ce841457ef31f94fb9c0418923811ca97d18
Jens Hartmann <hartmannje@cip.ifi.lmu.de>
2021-02-28 22:17:48 +0100

    Added Commentary

--- aco.pl
+++ aco.pl
@@ -1,10 +1,19 @@
+/* Jens Hartmann, Ant Colony Optomisation Algorithmus 28.02.2021
+Benutzung: siehe README.txt*/
+
 :- writeln("\nImported arcs.pl.\n"), ['arcs.pl'].
 /* Die Begriffe wurden verwendet im Sinne des usprünglichen Ant System Paper von
  Dorigo, Maniezzo, Colorni von 1996. */
 
-/* arcPref und arcTau werden als globale Variablen mittels assert angelegt (Teil 3).
- arcTau beschreibt die Intensität der Trails, diese werden nach dem Lauf einer 
-Ant aktualisiert (runAnt), haben jedoch erst Einfluss auf den nächsten Cycle. */
+/* Es werden die folgenden dynamischen globalen Variablen mittels assert 
+verwendet:
+ o arcPref : Preferenz ist die unnormierte Wahrscheinlichkeit einer Kante.
+ o arcTau : beschreibt die Intensität einer edge, diese werden nach dem Lauf 
+            einer Ameise aktualisiert (in runAnt), haben jedoch erst Einfluss 
+            im nächsten Cycle. 
+ o bestSol : Pfad und Kosten der derzeit besten gefunden Lösung.
+ */
+
 edgeC(X, Y, Weight) :- arcCost(X, Y, Weight) ; arcCost(Y, X, Weight).
 edgeP(X, Y, Pref) :- arcPref(X, Y, Pref) ; arcPref(Y, X, Pref).
 edgeT(X, Y, Tau) :- arcTau(X, Y, Tau) ; arcTau(Y, X, Tau).
@@ -17,31 +26,21 @@ beta(5).
 persistance(0.5). % Verdampfen der Trails
 constantQ(100). % Faktor für Tau
 totalAnts(100). % M Ants per Cycle.
-stableSolCountForLoopStop(20).
+stableSolCountForLoopStop(5).
 /******************************************************************************
 Teil 1: Boilerplate*/
+mainRunUntilStable :-
+    runInit, runUntilStable, writeSol, !.
 
-/* Initialisierung mit Anschließenden Start.*/ 
-main :-
-    allArcs(XYs), initTP(XYs),
-    assert(bestSol([], 2^31-1)),
-    mainContinue, !.
+/* Mehrere Cycles mit anschließender Ausgabe der Lösung.
+Kann nicht ohne zuvor initialisierter arcsPref und arcsTau verwendet werden
+(durch Aufruf von runInit) */
+mainRunCycleMultiple(Count) :-
+    runCycleMultiple(Count), writeSol, !.
 
-/* Start mit anschließender Ausgebe der Lösung. Kann wiederholt aufgerufen 
-werden, um die ausgegebene Lösung zu verbessern. */
-mainContinue :-
-    runACO,
-    bestSol(SolPath, CostNormed),
-    ['townNorm.pl'],    
-    normXY(NormXY),
-    Cost1 is (NormXY * CostNormed/100),
-    round(Cost1, SolCostOut, 2),
-    write("   Denormed Costs: "), writeln(SolCostOut),
-    write("   Solution Path: "), writeln(SolPath),
-    writeSol, !. 
-
-/**/
-runACO :-
+/* Eine vorgegebene Anzahl an Cycles wir ausgeführt, wurde eine bessere Lösung
+gefunden, dann wird die Schleife wiederholt*/
+runUntilStable :-
     bestSol(_, OldCost),
     stableSolCountForLoopStop(Count),
     runCycleMultiple(Count),
@@ -55,7 +54,7 @@ reRun(OldCost) :-
     bestSol(_, NewCost),
     write("   OldCost: "), write(OldCost),
     write(" NewCost: "), writeln(NewCost),
-    NewCost < OldCost, runACO.
+    NewCost < OldCost, runUntilStable.
 reRun(_). % In Case NewCost >= OldCost.
 
 runCycleMultiple(0).
@@ -84,7 +83,6 @@ runAnt :-
     path([Start|Rest], Path, C), updateBestSol(Path, C),
     last(Path, Last), updateT(Start, Last, C), updateT(Path, C).
 %    write("   Path: "), write(Path), write("   Cost: "), writeln(C).    
-
 
 /******************************************************************************
 Teil 2: Berechnen des Pfades mittels der Preferenzen.
@@ -118,11 +116,6 @@ choice(X, Unvisited, Y) :-
     sumP(NPs, 0, Pges),
     random(0, Pges, PTarget),
     seekNode(NPs, PTarget, 0, Y).
-/*    write("   Unvisited: "), writeln(Unvisited0),    
-    writeln("   NPs: "), pp(NPs,0),
-    write("   SumP: "), writeln(Pges),
-    write("   Tar: "), writeln(Tar),           
-    write("   Choosen: "), writeln(Y), writeln(""). */
 
 % Summe der Preferences:
 sumP([], Pges, Pges).
@@ -136,13 +129,18 @@ seekNode([_:P|NPs], PTarget, AccIn, Y) :-
     Acc1 is AccIn + P, seekNode(NPs, PTarget, Acc1, Y).
 
 /******************************************************************************
-Teil 3 Initialisieren und Updaten von Tau, Preference und Cost. */
+Teil 3 Initialisieren und Updaten von arcTau, arcPref und bestSol. */
 
-/* Initialisieren der Pheromonone  und Preferences von allen Edges.
+/* runInit/0 Initialisierung der Arcs und von bestSol.*/ 
+runInit :-
+    allArcs(XYs), runInit(XYs),
+    assert(bestSol([], 2^31-1)).
+
+/* runInit/1 Initialisieren der Pheromonone  und Preferences von allen Edges.
 Die Pheromone werden alle mit der selben Konstante initialisiert, der erste
 Cycle entspricht dem Greedy Algorithmus.*/
-initTP([]).
-initTP([X:Y|XYs]) :- assert(arcTau(X, Y, 1)), updateP(X, Y), initTP(XYs).
+runInit([]).
+runInit([X:Y|XYs]) :- assert(arcTau(X, Y, 1)), updateP(X, Y), runInit(XYs).
 
 /* updateT/1 Tau von allen Edges, die Teil des Pfades einer Ameise waren, werden
 aktualisiert.*/
@@ -231,9 +229,21 @@ dotEdge([X:Y|XYs]) :-
 /******************************************************************************
 Teil 5b: Standardmäßige Ausgebe der Lösung. */
 
-/* Die Kosten werden entnormiert und der Lösungspfad wird ausgegeben, um mittels
-Gnuplot anzeigt zu werden. Anschließend werden die Pheromone ausgegeben. */
+/* Start mit anschließender Ausgebe der Lösung. Kann wiederholt aufgerufen 
+werden, um die ausgegebene Lösung zu verbessern. */
 writeSol :-
+    bestSol(SolPath, CostNormed),
+    ['townNorm.pl'],    
+    normXY(NormXY),
+    Cost1 is (NormXY * CostNormed/100),
+    round(Cost1, SolCostOut, 2),
+    write("   Denormed Costs: "), writeln(SolCostOut),
+    write("   Solution Path: "), writeln(SolPath),
+    writeSolToFile, !. 
+
+/* Der Lösungspfad wird ausgegeben, um mittels Gnuplot anzeigt zu werden. 
+Anschließend werden die Pheromone ausgegeben. */
+writeSolToFile :-
     bestSol(Path, _),
     last(Path, Last), % Der Startknoten kommt zweimal im .dat vor
     tell('tsp_solution.dat'),
